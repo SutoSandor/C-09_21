067499d74923e3d08d780cc81f7e56872b17997c
SutoSandor <sandorsuto97@gmail.com>
2018-09-21 15:33:48 +0200

    Első feladat!

--- ConsoleApp0921/Program.cs
+++ ConsoleApp0921/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace ConsoleApp0921
 {
@@ -10,7 +11,21 @@ namespace ConsoleApp0921
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Első github push");
+            string[] tomb = new string[10];
+
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                Console.WriteLine("Kérem adjon meg egy számot:");
+                string s = Console.ReadLine();
+                tomb[i] = s;
+                s = "";
+            }
+            Console.WriteLine("A 10 számunk: ");
+            foreach (var i in tomb)
+            {
+                Console.Write(i+" ");
+            }
+            Console.ReadKey();
             
         }
     }
