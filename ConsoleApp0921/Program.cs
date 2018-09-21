4abebf3ae16280302862933da4a0cfb83a18528d
SutoSandor <sandorsuto97@gmail.com>
2018-09-21 15:41:09 +0200

    Harmadik feladat!

--- ConsoleApp0921/Program.cs
+++ ConsoleApp0921/Program.cs
@@ -27,6 +27,20 @@ namespace ConsoleApp0921
             {
                 Console.Write(i+" ");
             }
+            int max = Convert.ToInt32(tomb[0]);
+            int min = Convert.ToInt32(tomb[0]);
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                if (max< Convert.ToInt32(tomb[i]))
+                {
+                    max = Convert.ToInt32(tomb[i]);
+                }
+                else if (min > Convert.ToInt32(tomb[i]))
+                {
+                    min = Convert.ToInt32(tomb[i]);
+                }
+            }
+            Console.WriteLine("\nA minimum értékünk: " +min +", a maximum értékünk: " +max);
             Console.ReadKey();
             
         }
